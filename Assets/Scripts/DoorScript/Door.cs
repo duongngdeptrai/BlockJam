f7d299549903fa72f990b92251c5304b7c8037d3
Duongdeptrai <duongthcsvt7e@gmail.com>
2026-03-23 23:30:21 +0700

    pushmissing

--- Assets/Scripts/DoorScript/Door.cs
+++ Assets/Scripts/DoorScript/Door.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,6 +6,7 @@ public abstract class Door : MonoBehaviour
 {
     [SerializeField] protected int size ;
     [SerializeField] protected SpriteRenderer spriteRenderer;
+    [SerializeField] private List<ParticleSystem> particleSystems;
     protected ColorType colorType;
     protected Direction direction;
 
@@ -22,4 +24,17 @@ public abstract class Door : MonoBehaviour
         colorType = _colorType;
         spriteRenderer.color = ColorManager.GetColor(_colorType);
     } 
+    public void PlayParticles()
+    {
+        if (particleSystems == null) return;
+        foreach (var ps in particleSystems)
+        {
+            if (ps != null)
+            {
+                var main = ps.main;
+                main.startColor = ColorManager.GetColor(colorType);
+                ps.Play();
+            }
+        }
+    }
 }
